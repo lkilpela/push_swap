ef2a00c064950a050009c6ee7eab19092f081338
lumikil <kilpelainenlumi@gmail.com>
2024-02-12 23:04:58 +0200

    Update push_swap.h with necessary header files

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,13 +6,15 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 22:58:45 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/02/12 23:00:05 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/02/12 23:03:44 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+#include <unistd.h>
+#include <stdlib.h>
 #include "./lib/libft/build/libft.a"
 
 # endif
