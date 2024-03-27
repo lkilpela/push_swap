54a5cb194fe3e24e5daefe8800e3a66d6ea6b75a
lumikil <kilpelainenlumi@gmail.com>
2024-03-27 14:56:20 +0200

    Update push_swap.h with new includes and define a struct for stack nodes

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,15 +6,40 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 22:58:45 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/02/12 23:03:44 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/03/27 14:55:40 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#include <unistd.h>
+#include <stdbool.h>
+#include <limits.h>
 #include <stdlib.h>
-#include "./lib/libft/build/libft.a"
+#include "./lib/libft/include/libft.h"
+
+typedef struct s_stack_node
+{
+    int     nbr;
+    int     index;
+    int     pust_cost;
+    bool    above_median;
+    bool    cheapest;
+    struct s_stack_node *target_node;
+    struct s_stack_node *prev;
+    struct s_stack_node *next;
+}               t_stack_node;
+
+//Handle errors
+
+//Stack initiation
+
+//Nodes initiation
+
+//Stack utils
+
+//Commands
+
+//Algorithms
 
 # endif
