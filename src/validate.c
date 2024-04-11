67a07cc9073f3740b607e7ca7a741c1728425090
lumikil <kilpelainenlumi@gmail.com>
2024-04-11 09:33:54 +0300

    Fix missing function declaration in validate.c

--- src/validate.c
+++ src/validate.c
@@ -6,14 +6,15 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 09:02:10 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/11 09:30:39 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/11 09:33:49 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void    validate_argument(t_push_swap *ps)
+void	is_duplicate
+void	validate_argument(t_push_swap *ps)
 {
-    if (ps->argc < 2)
-        
+	if (ps->argc < 2)
+		error(ERR_NOT_ENOUGH_ARG);
 }
