a5605e9f835a4f929feabbe8914c08accc1dfed5
lumikil <kilpelainenlumi@gmail.com>
2024-04-11 09:20:03 +0300

    Update error.c with additional error messages and error handling logic

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 09:01:14 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/11 09:07:53 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/11 09:19:52 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,15 @@ static const char   *get_error_messages(int e)
 {
     static const char   *error_messages[] = {
         "No error",
-        "Error\n"
+        "Error\nNot enough arguments.",
+        "Error\nInvalid argument format or integer overflow.",
+        "Error\nEmpty argument.",
+        "Error\nDuplicate values.",
     }
 }
+
+	ERR_NOT_ENOUGH_ARG,
+	
+	ERR_INVALID_ARG,
+	ERR_EMPTY_ARG,
+	ERR_DUPLICATE
