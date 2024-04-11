5c088b771912265e59aee6c0e535267e567c450f
lumikil <kilpelainenlumi@gmail.com>
2024-04-11 15:05:18 +0300

    Update error.c to fix error handling and printing

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 09:01:14 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/11 12:50:49 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/11 15:05:01 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,13 @@ static const char	*get_error_messages(int e)
 	return (error_messages[e]);
 }
 
-void	error(int e)
+void	error_args(int e, const char *str)
+{
+	print_error(e, str);
+	exit(EXIT_FAILURE);
+}
+
+void	print_error(int e, const char *str)
 {
 	const char	*message;
 
@@ -36,8 +42,13 @@ void	error(int e)
 		message = get_error_messages(e);
 		ft_putstr_fd("Error\n", STDERR_FILENO);
 		ft_putstr_fd(message, STDERR_FILENO);
+		if (str)
+		{
+			ft_putstr_fd(": ", STDERR_FILENO);
+			ft_putstr_fd(str, STDERR_FILENO);
+		}
 	}
 	else
 		ft_putstr_fd ("Unknown error", STDERR_FILENO);
-	exit(EXIT_FAILURE);
+	write(2, "\n", 1);
 }
