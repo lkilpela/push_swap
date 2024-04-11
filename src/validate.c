fec305cfaa26290c5ad181f55a1728ca8d62aa17
lumikil <kilpelainenlumi@gmail.com>
2024-04-11 15:44:41 +0300

    Fix argument validation logic in validate.c

--- src/validate.c
+++ src/validate.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 09:02:10 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/11 15:22:32 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/11 15:44:29 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,37 +34,42 @@ static void	is_duplicate(t_push_swap *ps)
 	if (flg)
 		error(ERR_DUPLICATE);
 }
-
-static int	arg_has_alpha(char *str)
+/* returns 1, the string contains an alphabetic character. 
+returns 0, the string does not contain any alphabetic characters.*/
+static int	check_alpha(char *str)
 {
 	if (*str == '-' || *str == '+')
         str++;
 	while (*str)
 	{
-		if (!ft_isdigit(*str))
+		if (ft_isalpha(*str))
 			return (1);
 		str++;
 	}
 	return (0);
 }
+
 void	validate_argument(t_push_swap *ps)
 {
 	int		i;
 	t_atoi	check;
 
 	i = 1;
-	while (i < ps->argc)
+	if (ps->argc > 2)
 	{
-		if (ps->argc < 2)
-			error(ERR_NOT_ENOUGH_ARG);
-		if (ps->argv[1][0] == '\0')
-			error(ERR_EMPTY_ARG);
-		is_duplicate(ps);
-		if (arg_has_alpha(ps->argv[i]))
-			error(ERR_FORMAT);
-		check = check_atoi_overflow(ps->argv[i]);
-		if (!check.valid)
-			error(ERR_INT_OVERFLOW);
-		i++;
+		while (i < ps->argc)
+		{
+			check = check_atoi_overflow(ps->argv[i]);
+			if (ps->argv[1][0] == '\0')
+				error(ERR_EMPTY_ARG);
+			is_duplicate(ps);
+			if (!check_alpha(ps->argv[i]))
+				error(ERR_FORMAT);
+			if (check.valid)
+				error(ERR_INT_OVERFLOW);
+			i++;
+		}
 	}
+	else
+		error(ERR_NOT_ENOUGH_ARG);
 }
