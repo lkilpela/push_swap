a267c93eb3f1ab501d79944895808cccccd7f245
lumikil <kilpelainenlumi@gmail.com>
2024-04-11 14:47:31 +0300

    Refactor utils.c

--- src/utils.c
+++ src/utils.c
@@ -6,13 +6,13 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 14:18:43 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/11 14:33:38 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/11 14:47:14 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	ft_is_overflow(long long result, int sign, int digit)
+static int	is_overflow(long long result, int sign, int digit)
 {
 	if ((sign == 1 && result > (INT_MAX - digit) / 10)
 		|| (sign == -1 && result < (INT_MIN + digit) / 10))
@@ -20,16 +20,15 @@ static int	ft_is_overflow(long long result, int sign, int digit)
 	return (0);
 }
 
-t_atoi	check_atoi_overflow(const char *str)
+t_atoi	new_atoi(const char *str)
 {
     int			sign;
 	int			digit;
     long long	result;
-	t_atoi		i;
+	t_atoi		check;
     
 	sign = 1;
 	result = 0;
-	
 	while (ft_isspace(*str))
 		str++;
 	if (*str == '-')
@@ -39,15 +38,15 @@ t_atoi	check_atoi_overflow(const char *str)
 	while (ft_isdigit(*str))
 	{
 		digit = *str - '0';
-		if (ft_is_overflow(result, sign, digit))
+		if (is_overflow(result, sign, digit))
 		{
-			i.valid = 0;
-			return (i);
+			check.valid = 0;
+			return (check);
 		}
 		result = (result * 10) + (digit * sign);
 		str++;
 	}
-	i.valid = 1;
-	i.number = (int)result;
-	return (result);
+	check.valid = 1;
+	check.number = (int)result;
+	return (check);
 }
