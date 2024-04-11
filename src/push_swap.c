f66158b4a88b13a754bc577ba5ef05d12c05be24
lumikil <kilpelainenlumi@gmail.com>
2024-04-11 08:38:22 +0300

    Add main function to push_swap.c

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,9 +6,19 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 14:57:54 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/03/27 14:58:11 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/11 08:32:29 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int main(int argc, char **argv)
+{
+    t_stack_node    *a;
+    t_stack_node    *b;
+    
+    a = NULL;
+    b = NULL;
+
+    
+}
