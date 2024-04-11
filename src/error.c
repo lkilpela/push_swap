e105da120314a1ec13cc4f7d862cbb727c1abd5a
lumikil <kilpelainenlumi@gmail.com>
2024-04-11 09:31:40 +0300

    fix norm

--- src/error.c
+++ src/error.c
@@ -6,37 +6,37 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 09:01:14 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/11 09:31:00 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/11 09:31:37 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static const char   *get_error_messages(int e)
+static const char	*get_error_messages(int e)
 {
-    static const char   *error_messages[] = {
-        "No error",
-        "Not enough arguments.",
-        "Invalid argument format or integer overflow.",
-        "Empty argument.",
-        "Duplicate values.",
-        "Last error"
-    };
-    
-    return (error_messages[e]);
+	static const char   *error_messages[] = {
+		"No error",
+		"Not enough arguments.",
+		"Invalid argument format or integer overflow.",
+		"Empty argument.",
+		"Duplicate values.",
+		"Last error"
+	};
+
+	return (error_messages[e]);
 }
 
-void    error(int e)
+void	error(int e)
 {
-    const char  *message;
+	const char	*message;
 
-    if (e >= 0 || e < LAST_ERROR)
-    {
-        message = get_error_messages(e);
-        ft_putstr_fd("Error\n", STDERR_FILENO);
-        ft_putstr_fd(message, STDERR_FILENO);
-    }
-    else
+	if (e >= 0 || e < LAST_ERROR)
+	{
+		message = get_error_messages(e);
+		ft_putstr_fd("Error\n", STDERR_FILENO);
+		ft_putstr_fd(message, STDERR_FILENO);
+	}
+	else
 		ft_putstr_fd ("Unknown error", STDERR_FILENO);
-    exit(EXIT_FAILURE);
+	exit(EXIT_FAILURE);
 }
