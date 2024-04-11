9fb3aaa405a786ccf2cdb81562ebf432ce2e3c50
lumikil <kilpelainenlumi@gmail.com>
2024-04-11 12:32:58 +0300

    fix norm

--- src/init.c
+++ src/init.c
@@ -6,14 +6,14 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 11:34:59 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/11 11:36:22 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/11 12:31:52 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void    init_push_swap(int argc, char **argv, t_push_swap *ps)
+void	init_push_swap(int argc, char **argv, t_push_swap *ps)
 {
-    ps->argc = argc;
-    ps->argv = argv;
+	ps->argc = argc;
+	ps->argv = argv;
 }
