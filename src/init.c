a771aeedf058994356115f820fb828d887c4ebe0
lumikil <kilpelainenlumi@gmail.com>
2024-04-13 20:28:11 +0300

    Refactor initialization logic in init.c and push_swap.c

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 11:34:59 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/13 16:39:11 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/13 20:27:44 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,26 @@ void	init_stack(t_stack *a, t_stack *b)
 	b->size = 0;
 }
 
-void	init_stack_node(t_stack_node *node)
+void	init_node_a(int nbr, t_stack_node *a)
 {
-	node->nbr = 0;
-	node->index = 0;
-	node->pust_cost = 0;
-	node->above_median = 0;
-	node->cheapest = 0;
-	node->target_node = NULL;
-	node->next = NULL;
-	node->prev = NULL;
+	a->nbr = nbr;
+	a->index = -1;
+	a->pust_cost = -1;
+	a->above_median = false;
+	a->cheapest = false;
+	a->target_node = NULL;
+	a->next = NULL;
+	a->prev = NULL;
+}
+
+void	init_node_b(int nbr, t_stack_node *b)
+{
+	b->nbr = nbr;
+	b->index = -1;
+	b->pust_cost = -1;
+	b->above_median = false;
+	b->cheapest = false;
+	b->target_node = NULL;
+	b->next = NULL;
+	b->prev = NULL;
 }
