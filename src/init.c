fff696ac82948d391be21df5569dfa094915f9d9
lumikil <kilpelainenlumi@gmail.com>
2024-04-13 14:46:02 +0300

    Update init_push_swap function in init.c

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 11:34:59 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/12 08:00:56 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/13 14:44:41 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	init_push_swap(int argc, char **argv, t_push_swap *ps)
 {
 	ps->argc = argc;
 	ps->argv = argv;
+	ps->arg = NULL;
+	ps->tab = NULL;
 }
 
 void	init_stack(t_stack *a, t_stack *b)
