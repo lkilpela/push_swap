85c41138dc3672e52ce6d2ce8a8c44012c9a7877
lumikil <kilpelainenlumi@gmail.com>
2024-04-13 21:23:29 +0300

    Refactor current_index function and add set_target_a function in init_a_to_b.c

--- src/init_a_to_b.c
+++ src/init_a_to_b.c
@@ -6,13 +6,59 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 20:28:24 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/13 20:29:17 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/13 21:22:54 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void    current_index(t_stack *s)
+//assign an index to each node in a stack
+//set above_median based on whether the node's index is above/below median index
+void	current_index(t_stack_node *stack)
 {
-    
+	int i;
+	int median;
+
+	i = 0;
+	if (!stack)
+		return;
+	median = ft_lstsize(stack) / 2;
+	while (stack)
+	{
+		stack->index = i;
+		if (i < median)
+			stack->above_median = true;
+		else
+			stack->above_median = false;
+		stack = stack->next;
+		i++;
+	}
+}
+// Find 'a' node's target in stack 'b'
+static void	set_tartget_a(t_stack_node *a, t_stack_node *b)
+{
+	t_stack_node	*current_b;
+	t_stack_node	*target_node;
+	long			closest_smaller_number;
+
+	while(a)
+	{
+		closest_smaller_number = LONG_MIN;
+		current_b = b;
+		while (current_b)
+		{
+			if (current_b->nbr < a->nbr &&
+				current_b->number > closest_smaller_number)
+			{
+				closest_smaller_number = current_b->nbr;
+				target_node = current_b;
+			}
+			current_b = current_b->next;
+		}
+		if (closest_smaller_number == LONG_MIN)
+			a->target_node = find_max(b);
+		else
+			a->target_node = target_node;
+		a = a->next;
+	}
 }
