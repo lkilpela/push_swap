587bd10b7617a37354e2f81db8b6fb708b5dba07
lumikil <kilpelainenlumi@gmail.com>
2024-04-14 13:18:58 +0300

    Refactor push function in push.c

--- src/commands/push.c
+++ src/commands/push.c
@@ -6,9 +6,28 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 12:48:34 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/14 12:49:40 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/14 13:14:17 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static void ft_dlstadd_front(t_stack_node **lst, t_stack_node *new)
+{
+    if (new)
+    {
+        new->next = *lst;
+        if (*lst)
+            (*lst)->prev = new;
+        *lst = new;
+    }
+}
+
+static void ft_dlsadd_back(t_stack_node **lst, t_stack_node *new)
+{
+    
+}
+static void push(t_stack_node **dst, t_stack_node **src)
+{
+    
+}
