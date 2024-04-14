533847eadc72423161fe337b6f2fe95d080a9063
lumikil <kilpelainenlumi@gmail.com>
2024-04-14 12:46:20 +0300

    fix norm

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,21 +6,20 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 22:58:45 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/14 12:33:11 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/14 12:46:07 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#include "../lib/libft/include/libft.h"
+# include "../lib/libft/include/libft.h"
 
-#include <stdbool.h>
-#include <limits.h> // INT_MIN, INT_MAX
-#include <unistd.h> // read, write
-#include <stdlib.h> // malloc, free, exit
-#include <stdio.h>
-
+# include <stdbool.h>
+# include <limits.h> // INT_MIN, INT_MAX
+# include <unistd.h> // read, write
+# include <stdlib.h> // malloc, free, exit
+# include <stdio.h>
 
 enum	e_error
 {
@@ -41,60 +40,58 @@ typedef struct s_push_swap
 	char	**tab;
 }			t_push_swap;
 
-typedef struct	s_atoi
+typedef struct s_atoi
 {
 	int	valid;
 	int	number;
 }				t_atoi;
 
-typedef struct	s_stack_node
+typedef struct s_stack_node
 {
-	int		nbr;// The integer value of the node.
-	int		index;//The position of the node in the stack.
-	int		push_cost;//The cost of pushing this node onto the stack. This could be used in the sorting algorithm to decide which node to push.
-	bool	above_median;//A boolean flag indicating whether the node's value is above the median. This could be used in the sorting algorithm to decide which nodes to push or pop.
-	bool	cheapest;//A boolean flag indicating whether this node is the cheapest to push. This could be used in the sorting algorithm to decide which node to push.
-	struct s_stack_node *target_node;// a pointer to another t_stack_node. This could be used to keep track of a target node in the sorting algorithm.
-	struct s_stack_node *prev;// a pointer to the previous t_stack_node in a linked list
-	struct s_stack_node *next;// a pointer to the next t_stack_node in a linked list
+	int					nbr;// The integer value of the node.
+	int					index;//The position of the node in the stack.
+	int					push_cost;//The cost of pushing this node onto the stack
+					//This could be used in the sorting algorithm to decide 
+					//which node to push.
+	bool				above_median;//A boolean flag indicating whether the 
+									//node's value is above the median. 
+									//This could be used in the sorting 
+									//algorithm to decide which nodes to push 
+									//or pop.
+	bool				cheapest;//A boolean flag indicating whether this node 
+								//is the cheapest to push. This could be used 
+								//in the sorting algorithm to decide which node
+								//to push.
+	struct s_stack_node	*target_node;// a pointer to another t_stack_node.
+									//This could be used to keep track of 
+									//a target node in the sorting algorithm.
+	struct s_stack_node	*prev;// a pointer to the previous t_stack_node in a 
+							//linked list
+	struct s_stack_node	*next;// a pointer to the next t_stack_node in a 
+								//linked list
 }				t_stack_node;
 
-/*typedef struct s_stack
-{
-	t_stack_node	*top;// a pointer to the top node in the stack. This is used to easily add elements to and remove elements from the top of the stack.
-	int				size;//the number of elements in the stack. This is used to quickly check if the stack is empty (size is 0) or to get the number of elements in the stack.
-}				t_stack;*/
-
 //Handle errors
-int	validate_argument(t_push_swap *ps);
-void	error(int e);
-
+int				validate_argument(t_push_swap *ps);
+void			error(int e);
 // argument initiation
-void    init_push_swap(int argc, char **argv, t_push_swap *ps);
-//void	init_stack(t_stack *a, t_stack *b);
-
-void	mark_median(t_stack_node *n);
-
+void			init_push_swap(int argc, char **argv, t_push_swap *ps);
+void			mark_median(t_stack_node *n);
 //Stack preparation
+t_stack_node	*get_cheapest(t_stack_node *n);
+void			prep_push(t_stack_node **stack, t_stack_node *top,
+					char stack_name);
+//Nodes initiation
+void			init_node_a(int nbr, t_stack_node *a);
+void			init_node_b(int nbr, t_stack_node *a);
 void			prepare_nodes_a(t_stack_node *a, t_stack_node *b);
 void			prepare_nodes_b(t_stack_node *a, t_stack_node *b);
-t_stack_node    *get_cheapest(t_stack_node *n);
-
-//Nodes initiation
-void	init_node_a(int nbr, t_stack_node *a);
-void	init_node_b(int nbr, t_stack_node *a);
-
-
 //Commands
-
 //Algorithms
-
 //Utils
 t_atoi			check_atoi_overflow(const char *str);
 t_stack_node	*find_max(t_stack_node *n);
 t_stack_node	*find_min(t_stack_node *n);
-
 //Handle memory
-void    free_ptrs(char **array);
-
-# endif
+void			free_ptrs(char **array);
+#endif
--- src/prepare_stack.c
+++ src/prepare_stack.c
@@ -6,21 +6,42 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 12:22:30 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/14 12:28:13 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/14 12:36:33 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_stack_node    *get_cheapest(t_stack_node *n)
+t_stack_node	*get_cheapest(t_stack_node *n)
 {
-    if (!n)
-        return (NULL);
-    while (n)
-    {
-        if (n->cheapest)
-            return (n);
-        n = n->next;        
-    }
-    return (NULL);
+	if (!n)
+		return (NULL);
+	while (n)
+	{
+		if (n->cheapest)
+			return (n);
+		n = n->next;        
+	}
+	return (NULL);
+}
+
+void	prep_push(t_stack_node **stack, t_stack_node *top, char stack_name)
+{
+	while (*stack != top)
+	{
+		if (stack_name == 'a')
+		{
+			if (top->above_median)
+				ra(stack, false);
+			else
+				rra(stack, false);
+		}
+		else if (stack_name == 'b')
+		{
+			if (top->above_median)
+				rb(stack, false);
+			else
+				rrb(stack, false);
+		}
+	}
 }
