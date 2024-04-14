f7d5d11e4457d94c2be8524f44ccfe67cc7d4475
lumikil <kilpelainenlumi@gmail.com>
2024-04-14 09:44:38 +0300

    Refactor initialization logic in init_a_to_b.c and utils.c

--- src/init_a_to_b.c
+++ src/init_a_to_b.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 20:28:24 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/14 09:29:30 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/14 09:44:33 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,6 +64,7 @@ static void	set_tartget_a(t_stack_node *a, t_stack_node *b)
 	}
 }
 
+static void	analyse_cost_a(t_stack_node *a, t_stack_node *b)
 {
 	int	size_a;
 	int	size_b;
@@ -82,8 +83,6 @@ static void	set_tartget_a(t_stack_node *a, t_stack_node *b)
 		a = a->next;
 	}
 }
-
-
 
 void	init_nodes_a(t_stack_node *a, t_stack_node *b)
 {
