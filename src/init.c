a7845be6204e28afb8735651a9611752ea600c96
lumikil <kilpelainenlumi@gmail.com>
2024-04-14 22:45:19 +0300

    Refactor node initialization and stack sorting functions

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 11:34:59 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/14 22:34:29 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/14 22:44:05 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,19 @@ void	init_push_swap(int argc, char **argv, t_push_swap *ps)
 	ps->tab = NULL;
 }
 
-void	init_node_a(int nbr, t_stack_node *a)
+void	init_node_a(t_push_swap *ps, char **argv)
 {
-	a->nbr = nbr;
-	a->index = -1;
-	a->push_cost = -1;
-	a->above_median = false;
-	a->cheapest = false;
-	a->target_node = NULL;
-	a->next = NULL;
-	a->prev = NULL;
+	int	i = 1;
+	while (argv[i])
+	{
+		ps->a->nbr = ft_atoi(argv[i]);
+		ps->a->index = -1;
+		ps->a->push_cost = -1;
+		ps->a->above_median = false;
+		ps->a->cheapest = false;
+		ps->a->target_node = NULL;
+		ps->a->next = NULL;
+		ps->a->prev = NULL;
+		i++;
+	}
 }
