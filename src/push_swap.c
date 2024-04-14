2a921e62a54ad0077dc821e696b46dcf2405c6fa
lumikil <kilpelainenlumi@gmail.com>
2024-04-14 11:25:21 +0300

    Fix initialization bug in push_swap.c

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 14:57:54 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/13 16:54:24 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/14 11:23:50 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ int main(int argc, char **argv)
 	t_push_swap		ps;
 
 	init_push_swap(argc, argv, &ps);
-	init_stack(&ps.a, &ps.b);
 	validate_argument(&ps);
 	
 	return (0);
