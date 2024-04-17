d8bb98fd877065e8fed92c06736d32880452c91a
Lumi K <149065128+lkilpela@users.noreply.github.com>
2024-04-17 09:43:24 +0000

    Refactor sort_stacks.c for improved readability and performance

--- src/sort_stacks.c
+++ src/sort_stacks.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 13:22:40 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/17 08:04:14 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/17 09:42:49 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,8 +39,13 @@ static void	move_a_to_b(t_stack_node **a, t_stack_node **b)
 	t_stack_node	*chepeast_node;
 
 	chepeast_node = get_cheapest(*a);
-	if (chepeast_node->above_median && chepeast_node->target_node->above_median)
+	if (chepeast_node->above_median
+		&& chepeast_node->target_node->above_median)
 		rotate_both(a, b, chepeast_node);
+	else if (!(chepeast_node->above_median)
+			&& (!chepeast_node->target_node->above_median))
+		reverse_both(a, b, chepeast_node);
+	
 }
 
 // Check if stack is sorted in ascending order
@@ -57,10 +62,17 @@ bool	stack_sorted(t_stack_node *head)
 	return (true);
 }
 
-void	sort_stacks(t_stack_node *a, t_stack_node *b)
+void	sort_stacks(t_stack_node **a, t_stack_node **b)
 {
 	int	size_a;
 
-	size_a = ft_stacksize(a);
-	if ()
+	size_a = ft_stacksize(*a);
+	if (size_a-- > 3 && !stack_sorted(*a))
+		pa(b, a, false);
+	if (size_a-- > 3 && !stack_sorted(*a))
+		pa(b, a, false);
+	while (size_a-- > 3 && !stack_sorted(*a))
+	{
+		prepare_nodes_a(*a, *b);
+	}
 }
