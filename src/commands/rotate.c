757046b6dac054d04aa80ff567ba13d43961289d
lkilpela <lkilpela@student.hive.fi>
2024-04-18 16:42:54 +0300

    update rotate

--- src/commands/rotate.c
+++ src/commands/rotate.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 17:54:57 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/17 21:57:01 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/18 16:42:36 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ static void	rotate(t_stack_node **head)
 	if (!*head || !(*head)->next)
 		return ;
 	last_node = stack_last(*head);
+	last_node->prev->next = NULL; // Detach last node from end of list
 	last_node->next = *head;
-	*head = (*head)->next;
+	(*head)->prev = last_node;
+	(*head) = last_node;
 	(*head)->prev = NULL;
-	last_node->next->prev = last_node;
-	last_node->next->next = NULL;
 }
 
  // Rotate the top `a` node to the bottom of the stack, and print the instruction
