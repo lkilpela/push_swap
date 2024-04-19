8e0759b2362a808209ef43a75632550f911babbe
lumikil <kilpelainenlumi@gmail.com>
2024-04-19 10:23:32 +0300

    Update is_overflow function in utils.c to fix potential integer overflow issue

--- src/utils.c
+++ src/utils.c
@@ -6,51 +6,11 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 14:18:43 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/18 21:17:43 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/19 10:23:24 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-static int	is_overflow(long long result, int sign, int digit)
-{
-	if ((sign == 1 && result > INT_MAX / 10 - digit)
-		|| (sign == -1 && result < INT_MIN / 10 + digit))
-		return (1);
-	return (0);
-}
-
-t_atol	check_atol_overflow(const char *str)
-{
-	int			sign;
-	int			digit;
-	long long	result;
-	t_atol		check;
-
-	sign = 1;
-	result = 0;
-	while (ft_isspace(*str))
-		str++;
-	if (*str == '-')
-		sign = -1;
-	if (*str == '+' || *str == '-')
-		str++;
-	while (ft_isdigit(*str))
-	{
-		digit = *str - '0';
-		if (is_overflow(result, sign, digit))
-		{
-			check.valid = 1;
-			return (check);
-		}
-		result = (result * 10) + (digit * sign);
-		str++;
-	}
-	check.valid = 0;
-	check.number = (int)result;
-	return (check);
-}
-
 // Search a stack and returns the node with the biggest number
 t_stack_node	*find_max(t_stack_node *head)
 {
