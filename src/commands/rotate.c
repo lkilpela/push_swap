6a974d96ed61cfdf85759f3b246c053daa02fbcc
lumikil <kilpelainenlumi@gmail.com>
2024-04-19 12:35:18 +0300

    Update launch.json to use different arguments in .vscode/launch.json

--- src/commands/rotate.c
+++ src/commands/rotate.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 17:54:57 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/19 12:04:54 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/19 12:34:38 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,10 @@ static void	rotate(t_stack_node **head)
 	if (!*head || !(*head)->next)
 		return ;
 	last_node = stack_last(*head);
-	last_node->prev->next = NULL;
-	last_node->next = *head;
 	(*head)->prev = last_node;
-	(*head) = last_node;
-	(*head)->prev = NULL;
+	last_node->next = *head;
+	(*head)->next->prev  = NULL;
+	(*head)->next = NULL;
 }
 
  // Rotate the top `a` node to the bottom of the stack & print intruction
