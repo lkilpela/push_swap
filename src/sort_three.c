0b7d821d88347541a36da2862aa7db6f4c2defb9
lumikil <kilpelainenlumi@gmail.com>
2024-04-19 13:58:39 +0300

    Refactor sort_three function in sort_three.c and update last updated date

--- src/sort_three.c
+++ src/sort_three.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 13:22:55 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/19 13:58:06 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/19 13:58:34 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,6 @@
 // Sort a stack with three nodes
 void	sort_three(t_stack_node **a)
 {
-	t_stack_node	*min_node;
-	t_stack_node	*max_node;
-
 	if ((*a)->nbr > (*a)->next->nbr)
 		sa(a, false);
 	if ((*a)->next->nbr > (*a)->next->next->nbr)
