f1bd47de612bc1218c44c386ecc40a737a37178c
lumikil <kilpelainenlumi@gmail.com>
2024-04-19 13:58:20 +0300

    Refactor sort_three function in sort_three.c

--- src/sort_three.c
+++ src/sort_three.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 13:22:55 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/19 13:41:44 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/19 13:58:06 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,10 @@ void	sort_three(t_stack_node **a)
 	t_stack_node	*min_node;
 	t_stack_node	*max_node;
 
-	mark_median(*a);
-	min_node = find_min(*a);
-	max_node = find_max(*a);
-	if (min_node == *a)
-	{
-		rra(a, false);
+	if ((*a)->nbr > (*a)->next->nbr)
 		sa(a, false);
-	}
-	else if (max_node == *a)
-	{
-		ra(a, false);
-		if (stack_sorted(*a))
-			sa(a, false);
-	}
-	else
-	{
-		if(max_node->above_median == true)
-			rra(a, false);
-		else
-			sa(a, false);
-	}
+	if ((*a)->next->nbr > (*a)->next->next->nbr)
+		rra(a, false);
+	if ((*a)->nbr > (*a)->next->nbr)
+		sa(a, false);
 }
