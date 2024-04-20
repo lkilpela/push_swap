dbab7c5e59417fad2ba9de33fcd4c27e34f75a55
lumikil <kilpelainenlumi@gmail.com>
2024-04-20 22:15:14 +0300

    u

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 22:05:49 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/20 22:09:50 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/20 22:15:11 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,3 +18,15 @@ void	free_stack(t_stack *s)
 		free(s->str);
 }
 
+void	grow(t_stack *s)
+{
+	int	*str;
+
+	str = malloc(sizeof(int) * s->capacity);
+	if (!str)
+		exit(1);
+	ft_memcpy(str, s->str, sizeof(int) * s->capacity);
+	free(s->str);
+	s->str = str;
+	s->capacity *= 2;
+}
