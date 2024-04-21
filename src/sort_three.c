3bd3320d29bc59f4bdde87abab8e238c0aa53826
lumikil <kilpelainenlumi@gmail.com>
2024-04-21 23:32:46 +0300

    u

--- src/sort_three.c
+++ src/sort_three.c
@@ -6,11 +6,19 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 22:56:46 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/21 23:27:25 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/21 23:32:31 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+/*
+case1 : [2,1,3]->sa->[1,2,3].
+case2 : [3,2,1]->sa->[2,3,1]->rra->[1,2,3].
+case3 : [3,1,2]->ra->[1,2,3].
+case4 : [1,3,2]->sa->[3,1,2]->ra->[1,2,3].
+case5 : [2,3,1]->rra->[1,2,3].
+*/
 
 void	sort_three(t_stack* s)
 {
@@ -18,42 +26,22 @@ void	sort_three(t_stack* s)
     int b = s->array[1];
     int c = s->array[2];
 
-    if ((a < b && b < c) || (b < c && c < a))
+	if (a > b && a < c)        // case 1: [2,1,3]
+        sa(s);
+    else if (a > b && b > c)   // case 2: [3,2,1]
     {
-        if (b < c && c < a)
-			swap(s);
+        sa(s);
+        rra(s);
     }
-    else if (c < a && a < b)
-	{
-        rotate(s);
-		ft_printf("ra\n");
-	}
-    else if (a < c && c < b)
-	{
-        reverse_rotate(s);
-		ft_printf("rra\n");
-	}
-    else if (b < a && a < c)
+    else if (a > b && b < c)   // case 3: [3,1,2]
+        ra(s);
+    else if (a < b && b > c)   // case 4: [1,3,2]
     {
-        swap(s);
-		ft_printf("swap\n");
-        rotate(s);
-		ft_printf("rotate\n");
-	}
-	else if (b < c && c < a)
-	{
-		swap(s);
-		ft_printf("swap\n");
-		reverse_rotate(s);
-		ft_printf("rrotate\n");
+        sa(s);
+        ra(s);
     }
-    else if (c < b && b < a)
-    {
-		swap(s);
-		ft_printf("swap\n");
-		reverse_rotate(s);
-		ft_printf("rrotate\n");
-    }
+    else if (a < b && a > c)   // case 5: [2,3,1]
+        rra(s);
 }
 
 
