98afae6867b01b46a536f0b5ad9690cee881dc0b
lumikil <kilpelainenlumi@gmail.com>
2024-04-22 10:21:44 +0300

    u

--- src/sort_five.c
+++ src/sort_five.c
@@ -6,107 +6,34 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 23:35:42 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/04/21 23:45:41 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/04/22 10:21:38 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-https://github.com/gemartin99/Push_swap/blob/master/src/size5.c
-void	size4(t_stack *s)
+//Identify the two smallest numbers in the stack.
+// Move these two numbers to the top of the stack using the appropriate operations (ra, rra, sa).
+// Push these two numbers onto stack B (pb).
+void	rotate_smallest_to_top(t_stack *a, t_stack *b)
 {
-    int a = s->array[0];
-    int b = s->array[1];
-    int c = s->array[2];
-    int d = s->array[3];
+	int	smallest_i;
+	int	a_moves;
 
-	if (s->size == 4)
-	{
-		if (a < b && a < c && a < d && s->size == 4)
-			pb(s);
-		if (a > b && b < c && b < d && s->size == 4)
-		{
-			sa(s);
-			pb(s);
-		}
-		if (a > c && b > c && c < d && s->size == 4)
-		{
-			ra(s);
-			sa(s);
-			pb(s);
-		}
-		if (a > c && b > d && c > d && s->size == 4)
-		{
-			rra(s);
-			pb(s);
-		}
-	}
-	if (s->size == 3)
-		sort_three(s);
-	pa(s);
+	smallest_i = find_smallest(a->array, a->size);
+	a_moves = calculate_min_rotations(a, smallest_i);
+	rotate_stack("a", a, smallest_i, a_moves);
+	push_to_top(b, remove_top(a));
+	ft_printf("pb\n");
 }
-
-int	size5_2(t_stack *s, int i)
+// Sort three and push 2 numbers back from stack b to a
+void	sort_five(t_stack *a, t_stack *b)
 {
-    int a = s->array[0];
-    int b = s->array[1];
-    int c = s->array[2];
-    int d = s->array[3];
-    int e = s->array[4];
-
-	if (a > c && b > c && c < d && c < e && i == 0)
-	{
-		ra(s);
-		sa(s);
-		pb(s);
-		i++;
-	}
-	if (a > d && b > d && c > d && d < e && i == 0)
-	{
-		rra(s);
-		rra(s);
-		pb(s);
-		i++;
-	}
-	if (a > e && b > e && c > e && d > e && i == 0)
-	{
-		rra(s);
-		pb(s);
-		i++;
-	}
-	return (i);
-}
-
-void	size5(t_stack *s)
-{
-	int	i;
-    int a = s->array[0];
-    int b = s->array[1];
-    int c = s->array[2];
-    int d = s->array[3];
-    int e = s->array[4];
-
-	i = 0;
-	if (s->size == 5)
-	{
-		if (a < b && a < c && a < d && a < e && i == 0)
-		{
-			pb(s);
-			i++;
-		}
-		if (a > b && b < c && b < d && b < e && i == 0)
-		{
-			sa(s);
-			pb(s);
-			i++;
-		}
-		i = size5_2(s, d, i);
-	}
-	size4(s);
-	if (i == 1)
-	{
-		pa(s);
-		if (a > b)
-			sa(s);
-	}
+	rotate_smallest_to_top(a, b);
+	rotate_smallest_to_top(a, b);
+	sort_three(a);
+	push_to_top(a, remove_top(b));
+	push_to_top(a, remove_top(b));
+	ft_printf("pa\n");
+	ft_printf("pa\n");
 }
